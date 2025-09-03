95dbb3a710970ad63b353da325edab2b3b8fc130
Billi0121 <gafarovbilol2009@gmail.com>
2025-09-02 20:59:24 -0500

    Adding Paths

--- packing_system/packing/models.py
+++ packing_system/packing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.SlugField(max_length=15,unique=True)
+
+    def __str__(self):
+        return self.name
+    
--- packing_system/packing/views.py
+++ packing_system/packing/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'packing/index.html')
+    kitchen = Group.objects.get(pk=2)
+    livingroom = Group.objects.get(pk=3)
+    bedroom = Group.objects.get(pk=4)
+    context = {
+        'kitchen': kitchen,
+        'bedroom': bedroom,
+        'livingroom': livingroom,
+    }
+    return render(request, 'packing/index.html', context)
+
+def group(request, slug):
+    group = Group.objects.get(name=slug)
+    return render(request, 'packing/group.html')
