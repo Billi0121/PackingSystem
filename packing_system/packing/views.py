1792b6b6ffe04333500d1889cd7f7f53e83c9c89
Billi0121 <gafarovbilol2009@gmail.com>
2025-09-10 13:24:03 -0500

    Media

--- packing_system/packing/views.py
+++ packing_system/packing/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import *
+from .models import Group, Furniture
 
 # Create your views here.
 def index(request):
-    kitchen = Group.objects.get(pk=2)
-    livingroom = Group.objects.get(pk=3)
-    bedroom = Group.objects.get(pk=4)
+    kitchen = Group.objects.get(pk=3)
+    livingroom = Group.objects.get(pk=2)
+    bedroom = Group.objects.get(pk=1)
     context = {
         'kitchen': kitchen,
         'bedroom': bedroom,
@@ -15,8 +15,9 @@ def index(request):
 
 def group(request, slug):
     group = Group.objects.get(name=slug)
-    furniture = Furniture.objects.filter(Group=group)
+    furnitures = Furniture.objects.filter(Group=group)
     context = {
-        'furniture': furniture,
+        'furnitures': furnitures,
+        'group': group
     }
-    return render(request, 'packing/group.html')
+    return render(request, 'packing/group.html', context)
